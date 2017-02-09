3a886a1c40f8cb422ef58c3fd5118dca3f1eb257
Tamaized <ryanolt123@gmail.com>
2017-02-08 22:47:40 -0800

    wing template

--- src/main/java/Tamaized/CosmicReality/proxy/ClientProxy.java
+++ src/main/java/Tamaized/CosmicReality/proxy/ClientProxy.java
@@ -1,7 +1,14 @@
 package Tamaized.CosmicReality.proxy;
 
+import Tamaized.CosmicReality.client.event.RenderPlayer;
 import Tamaized.TamModized.proxy.AbstractProxy;
+import net.minecraftforge.common.MinecraftForge;
 
 public class ClientProxy extends AbstractProxy {
+
+	@Override
+	public void preInit() {
+		MinecraftForge.EVENT_BUS.register(new RenderPlayer());
+	}
 
 }
