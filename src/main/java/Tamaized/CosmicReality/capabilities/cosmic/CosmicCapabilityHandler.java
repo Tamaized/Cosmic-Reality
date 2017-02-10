3fc77e0ce3f1cadb61ead867f11d0e5628b2b2be
Tamaized <ryanolt123@gmail.com>
2017-02-10 15:04:37 -0800

    wing colors

--- src/main/java/Tamaized/CosmicReality/capabilities/cosmic/CosmicCapabilityHandler.java
+++ src/main/java/Tamaized/CosmicReality/capabilities/cosmic/CosmicCapabilityHandler.java
@@ -2,9 +2,9 @@ package Tamaized.CosmicReality.capabilities.cosmic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import Tamaized.CosmicReality.CosmicReality;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.Vec3d;
 
@@ -23,58 +23,71 @@ public class CosmicCapabilityHandler implements ICosmicCapability {
 		wingList.clear();
 
 		double z = -0.641;
+
+		Random rand = new Random();
 		
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 1.35F, z), null));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 1.35F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
 
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 1.35F, z), null));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
-		
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 1.25F, z), null));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 1.35F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
 
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 1.25F, z), null));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 1.25F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
 
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 1.15F, z), null));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 1.25F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
 
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 1.15F, z), null));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 1.15F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
 
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 1.05F, z), null));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 1.15F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
 
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 1.05F, z), null));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 1.05F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
 
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 0.95F, z), null));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 1.05F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
 
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 0.95F, z), null));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size()-1)));
-		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size()-1)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(-0.3F, 0.95F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.RIGHT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-0.7F, 0.95F, z), null, getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, length, -length), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(-length, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
+		wingList.add(new WingVector(WingVector.Side.LEFT, new Vec3d(0, -length, 0), wingList.get(wingList.size() - 1), getRandColor(rand)));
 
 		return this;
+	}
+
+	private int getRandColor(Random rand) {
+		//TODO: allow range to be set
+		int rangeRA = 0x00 + 1;// 0x00;
+		int rangeRB = 0x00;// 0xFFFFFF/2;
+		int rangeGA = 0xFF + 1;
+		int rangeGB = 0x00;// 0xFFFFFF/2;
+		int rangeBA = 0x00 + 1;
+		int rangeBB = 0xFF;// 0xFFFFFF/2;
+		return ((rand.nextInt(rangeRA) + rangeRB) << 16) + ((rand.nextInt(rangeGA) + rangeGB) << 8) + (rand.nextInt(rangeBA) + rangeBB);
 	}
 
 	@Override
@@ -85,9 +98,9 @@ public class CosmicCapabilityHandler implements ICosmicCapability {
 	@Override
 	public void update() {
 		tick++;
-		if (tick % 40 == 0) {
+		if (tick % 30 == 0) {
 			for (WingVector vec : wingList)
-				if (vec.getParent() != null) {
+				if (vec.getParent() != null && Math.floor(Math.random() * 2) == 0) {
 					vec.setPoint(new Vec3d((Math.random() * (length * (vec.getSide() == WingVector.Side.LEFT ? -1 : 1))), (Math.random() * (length)), (Math.random() * (-length))));
 				}
 			tick = 0;
@@ -105,11 +118,14 @@ public class CosmicCapabilityHandler implements ICosmicCapability {
 
 		private Vec3d point;
 		private Vec3d render;
+		private int color;
 
-		public WingVector(Side s, Vec3d vec, WingVector parent) {
+		public WingVector(Side s, Vec3d vec, WingVector parent, int color) {
 			side = s;
 			render = point = vec;
 			this.parent = parent;
+			this.color = color;
+			// System.out.println(Integer.toHexString(color));
 		}
 
 		public Side getSide() {
@@ -118,6 +134,10 @@ public class CosmicCapabilityHandler implements ICosmicCapability {
 
 		public WingVector getParent() {
 			return parent;
+		}
+
+		public int getColor() {
+			return color;
 		}
 
 		public void setPoint(Vec3d vec) {
--- src/main/java/Tamaized/CosmicReality/client/event/RenderPlayer.java
+++ src/main/java/Tamaized/CosmicReality/client/event/RenderPlayer.java
@@ -2,6 +2,7 @@ package Tamaized.CosmicReality.client.event;
 
 import org.lwjgl.opengl.GL11;
 
+import Tamaized.CosmicReality.CosmicReality;
 import Tamaized.CosmicReality.capabilities.CapabilityList;
 import Tamaized.CosmicReality.capabilities.cosmic.CosmicCapabilityHandler;
 import Tamaized.CosmicReality.capabilities.cosmic.ICosmicCapability;
@@ -9,13 +10,17 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class RenderPlayer {
+	
+	private static ResourceLocation TEXTURE = new ResourceLocation(CosmicReality.modid, "textures/fx/ray.png");
 
 	@SubscribeEvent
 	public void render(RenderPlayerEvent.Pre e) {
@@ -24,33 +29,32 @@ public class RenderPlayer {
 		Tessellator tessellator = Tessellator.getInstance();
 		VertexBuffer vertexbuffer = tessellator.getBuffer();
 
+//		Minecraft.getMinecraft().renderEngine.bindTexture(TEXTURE);
+		
 		GL11.glPushMatrix();
 		GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
 
-		GlStateManager.rotate(-player.prevRenderYawOffset, 0, 1, 0);
+		GlStateManager.rotate(-player.renderYawOffset, 0, 1, 0);
 		GL11.glTranslated(-player.posX, -player.posY, -player.posZ);
 		GL11.glDisable(GL11.GL_LIGHTING);
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		
+//		player.sendMessage(new TextComponentString(""+(0x000000 >> 16 & 0xFF)));
 
 		ICosmicCapability cap = player.getCapability(CapabilityList.COSMIC, null);
 		if (cap != null) {
-			/*
-			 * Vec3d pos7_1 = player.getPositionVector().addVector(-0.3F, 0.75F, -0.39); Vec3d pos7_2 = pos7_1.addVector(length, length, length); Vec3d pos7_3 = pos7_2.addVector(length, -length, 0); Vec3d pos7_4 = pos7_3.addVector(0, -length, 0); Vec3d pos8_1 = player.getPositionVector().addVector(-0.7F, 0.75F, -0.39); Vec3d pos8_2 = pos8_1.addVector(-length, length, length); Vec3d pos8_3 = pos8_2.addVector(-length, -length, 0); Vec3d pos8_4 = pos8_3.addVector(0, -length, 0);
-			 */
-
 			for (CosmicCapabilityHandler.WingVector vec : cap.getWingList()) {
 				vec.update();
-				if (vec.getParent() != null){
-					
-					drawLineWithGL(player.getPositionVector().add(vec.getParent().getRenderVector()), player.getPositionVector().add(vec.getRenderVector()));
+				if (vec.getParent() != null) {
+//					player.sendMessage(new TextComponentString(Integer.toHexString(vec.getColor() >> 16 & 0xFF)));
+//					GlStateManager.color(((vec.getColor() << 0) & 0xFF) / 255F, ((vec.getColor() << 8) & 0xFF) / 255F, (vec.getColor() & 0xFF) / 255F, 1);
+					GlStateManager.color((vec.getColor() >> 16 & 0xFF) / 255F, (vec.getColor() >> 8 & 0xFF) / 255F, (vec.getColor() & 0xFF) / 255F, 1);
+					drawLineWithGL(tessellator, vertexbuffer, player.getPositionVector().add(vec.getParent().getRenderVector()), player.getPositionVector().add(vec.getRenderVector()));
 				}
 			}
 
 		}
-		/*
-		 * drawLineWithGL(pos7_1, pos7_2); drawLineWithGL(pos7_2, pos7_3); drawLineWithGL(pos7_3, pos7_4); drawLineWithGL(pos8_1, pos8_2); drawLineWithGL(pos8_2, pos8_3); drawLineWithGL(pos8_3, pos8_4);
-		 */
-		GL11.glColor3f(1.0f, 1.0f, 1.0f);
+		GlStateManager.color(1, 1, 1, 1);
 		GL11.glPopAttrib();
 		GL11.glPopMatrix();
 	}
@@ -60,18 +64,17 @@ public class RenderPlayer {
 
 	}
 
-	private void drawLineWithGL(Vec3d blockA, Vec3d blockB) {
+	private void drawLineWithGL(Tessellator tessellator, VertexBuffer vertexbuffer, Vec3d blockA, Vec3d blockB) {
 		int d = Math.round((float) blockA.distanceTo(blockB) + 0.2f);
 		float oz = (blockA.xCoord - blockB.xCoord == 0 ? 0 : -1f / 16f);
 		float ox = (blockA.zCoord - blockB.zCoord == 0 ? 0 : 1f / 16f);
-		GL11.glBegin(GL11.GL_LINE_STRIP);
+		float size = 0.01F;
+		
+		GlStateManager.glBegin(3);
+		GL11.glVertex3d(blockA.xCoord + 0.5F, blockA.yCoord - 0.01F, blockA.zCoord + 0.5F);
+		GL11.glVertex3d(blockB.xCoord + 0.5F, blockB.yCoord - 0.01F, blockB.zCoord + 0.5F);
+		GlStateManager.glEnd();
 
-		GlStateManager.color(1, 0, 0, 1);
-		GL11.glVertex3d(blockA.xCoord + 0.5, blockA.yCoord - 0.01, blockA.zCoord + 0.5);
-		GlStateManager.color(0, 0, 1, 1);
-		GL11.glVertex3d(blockB.xCoord + 0.5, blockB.yCoord - 0.01, blockB.zCoord + 0.5);
-
-		GL11.glEnd();
 	}
 
 }
